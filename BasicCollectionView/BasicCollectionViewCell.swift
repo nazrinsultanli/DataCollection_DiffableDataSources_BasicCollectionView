982085ea8693f83eb33c94309ff0ac4aad3a0150
nazrinsultanli <nazrindolkhanova@gmail.com>
2025-02-24 19:30:26 +0400

    diffable applied

--- BasicCollectionView/BasicCollectionViewCell.swift
+++ BasicCollectionView/BasicCollectionViewCell.swift
@@ -5,9 +5,37 @@
 //  Created by Nazrin Sultanlı on 18.02.25.
 //
 
+//import UIKit
+//
+//class BasicCollectionViewCell: UICollectionViewCell {
+//    
+//    @IBOutlet weak var label: UILabel!
+//}
+
 import UIKit
 
 class BasicCollectionViewCell: UICollectionViewCell {
+    let label: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textAlignment = .center
+        lbl.textColor = .black
+        return lbl
+    }()
     
-    @IBOutlet weak var labelView: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- BasicCollectionView/BasicCollectionViewController.swift
+++ BasicCollectionView/BasicCollectionViewController.swift
@@ -7,20 +7,26 @@
 
 import UIKit
 
+enum Section: CaseIterable {
+    case main
+}
+
 private let reuseIdentifier = "BasicCell"
 private let items = [
-    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-    "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
-    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
-    "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
-    "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
-    "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
-    "Wisconsin", "Wyoming"
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
 ]
 var filteredItems: [String] = items
 
 class BasicCollectionViewController: UICollectionViewController, UISearchResultsUpdating {
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, String>!
+    var filteredItemsSnapshot: NSDiffableDataSourceSnapshot<Section, String> {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(filteredItems)
+        
+        return snapshot
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,19 +41,23 @@ class BasicCollectionViewController: UICollectionViewController, UISearchResults
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
+        createDataSource()
         collectionView.setCollectionViewLayout(generateAutoLayout(), animated: false)
+        
+        
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    func createDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<BasicCollectionViewCell, String> { cell, indexpath, itemIdentifier in
+            cell.label.text = itemIdentifier
+        }
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier  -> BasicCollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        dataSource.apply(filteredItemsSnapshot)
     }
-    */
+    
     private func generateAutoLayout() -> UICollectionViewLayout {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                               heightDimension: .fractionalHeight(1.0))
@@ -79,55 +89,9 @@ class BasicCollectionViewController: UICollectionViewController, UISearchResults
         } else{
             filteredItems = items
         }
-        collectionView.reloadData()
+        dataSource.apply(filteredItemsSnapshot, animatingDifferences: true)
     }
     
-
-
-    // MARK: UICollectionViewDataSource
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return filteredItems.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BasicCollectionViewCell
-        cell.labelView.text = filteredItems[indexPath.item]
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
 
 
